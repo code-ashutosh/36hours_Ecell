a23b4804a91933fd404bfbfe6b038bdd525224ba
code-ashutosh <in.ashutoshtrivedi@gmail.com>
2019-11-23 16:08:57 +0530

    added starter file

---
+++ main.js
@@ -0,0 +1,10 @@
+//js initialization of material components
+document.addEventListener('DOMContentLoaded', function(){
+
+  M.AutoInit();
+    // let elem =document.querySelectorAll('parallax');
+    // M.Parallax.init(elem,{
+
+    // });
+    
+});
